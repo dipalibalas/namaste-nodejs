97b4556fecadf95642c8c093f6a96776c0f593b7
dipalibalas <dipalimbalas@gmail.com>
2026-02-07 14:08:58 +0530

    completed season 1

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-let name = "Node JS 03";
-let a = 5;
-let b = 10;
-let c = a+b;
-console.log(name);
-console.log(c);
+require("./xyz.js"); //  one module into another
+
+// const util = require("node:util");
+
+const { calculateSum, calculateMultiply } = require("./calculate");
+
+const data = require("./data.json");
+
+console.log(data);
+
+var name = "Namaste NodeJS";
+
+var a = 10;
+
+var b = 20;
+
+calculateSum(a, b);
+calculateMultiply(a, b);
